d1792b416333037998cb64b6acf3499fa7a93f41
lilitoones <liliarevalovargas@gmail.com>
2022-07-05 21:23:27 -0500

    primer y segundo ejercicio introducción a la programación

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+//Crear una función con tres parámetros que sean números que se suman entre sí
+// Llamar a la función en el main y darle valores
+//inicio primer ejercicio introducción a la programación
+
+public class Main {
+       public static void main (String[] args) {
+           int param1 = 23;
+           int param2 = 14;
+           int param3 = 9;
+
+           var valor = suma(param1, param2, param3);
+
+     System.out.println(valor);
+    }
+    public static int suma(int a, int b, int c) {
+           return a+b+c;
+    }
+
+}
+//fin primer ejercicio introducción a la programación
+
+
+
+
+
+
+
+
---
+++ src/miCoche.java
@@ -0,0 +1,32 @@
+// Crear una clase coche
+// Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene
+// Una función que incremente el número de puertas que tiene el coche
+// Crear un objeto miCoche en el main y añadirle una puerta
+// Mostrar el número de puertas que tiene el objeto
+
+//inicio segundo ejercicio introducción a la programación
+
+public class miCoche {
+    private int numeroPuertas;
+
+    public miCoche(int numeroPuertas) {
+        this.numeroPuertas = numeroPuertas;
+
+    }
+
+    public void addPuertas(int numeroPuertas) {
+        this.numeroPuertas += numeroPuertas;
+    }
+
+    public void printInfo() {
+        System.out.println("Puertas:" + numeroPuertas);
+    }
+
+    public static void main(String[] args) {
+        miCoche coche = new miCoche(2);
+        coche.addPuertas(2);
+        coche.printInfo();
+    }
+}
+
+//fin segundo ejercicio introducción a la programación
